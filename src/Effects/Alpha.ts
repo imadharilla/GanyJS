b287d8324b80f84fc125690bd7c2e4382007611e
Aimad Harilla <imadharilla@gmail.com>
2021-10-19 10:29:17 +0100

    fix bug

--- src/Effects/Alpha.ts
+++ src/Effects/Alpha.ts
@@ -1,30 +1,17 @@
-import * as Nodes from 'three/examples/jsm/nodes/Nodes';
+import * as Nodes from "three/examples/jsm/nodes/Nodes";
 
-import {
-  Effect, Input, InputDimension
-} from '../EffectBlock';
+import { Effect, Input, InputDimension } from "../EffectBlock";
 
-import {
-  Block
-} from '../Block';
+import { Block } from "../Block";
 
-import {
-  Component
-} from '../Data';
+import { Component } from "../Data";
 
-import {
-  NodeOperation
-} from '../NodeMesh';
+import { NodeOperation } from "../NodeMesh";
 
-import {
-  IdentityNode
-} from '../utils/Nodes';
+import { IdentityNode } from "../utils/Nodes";
 
-
-export
-class Alpha extends Effect {
-
-  constructor (parent: Block, input: Input) {
+export class Alpha extends Effect {
+  constructor(parent: Block, input: Input) {
     super(parent, input);
 
     this.alphaNode = new IdentityNode(this.inputNode);
@@ -36,17 +23,17 @@ class Alpha extends Effect {
     this.sortTriangleIndices();
 
     // There is no new geometry specific to this effect, we forward the parent event
-    this.parent.on('change:geometry', () => {
+    this.parent.on("change:geometry", () => {
       this.sortTriangleIndices.bind(this);
 
-      this.trigger('change:geometry');
+      this.trigger("change:geometry");
     });
 
     this.initialized = true;
     this.updateMatrix();
   }
 
-  setInput(input?: Input) : void {
+  setInput(input?: Input): void {
     super.setInput(input);
 
     if (this.initialized) {
@@ -56,20 +43,20 @@ class Alpha extends Effect {
     }
   }
 
-  handleCameraMoveEnd (cameraPosition: THREE.Vector3) {
-    super.handleCameraMoveEnd(cameraPosition);
+  // handleCameraMoveEnd(cameraPosition: THREE.Vector3) {
+  //   super.handleCameraMoveEnd(cameraPosition);
 
-    this.sortTriangleIndices();
-  }
+  //   this.sortTriangleIndices();
+  // }
 
-  sortTriangleIndices () {
+  sortTriangleIndices() {
     // TODO: Throttle this call?
     for (const nodeMesh of this.meshes) {
       nodeMesh.sortTriangleIndices(this.lastCameraPosition);
     }
   }
 
-  get inputDimension () : InputDimension {
+  get inputDimension(): InputDimension {
     return 1;
   }
 
@@ -79,5 +66,4 @@ class Alpha extends Effect {
 
   protected inputs: [Component];
   protected inputNode: Nodes.Node;
-
 }
